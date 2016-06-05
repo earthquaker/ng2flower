c482152854fd71eb334a10c0e85bacff530b5d54
Elias Hansen <pe_lias@msn.com>
2016-06-05 18:08:15 +0200

    added admin route

--- src/app/ng2flower.component.ts
+++ src/app/ng2flower.component.ts
@@ -4,7 +4,9 @@ import { Component } from '@angular/core';
   moduleId: module.id,
   selector: 'ng2flower-app',
   templateUrl: 'ng2flower.component.html',
-  styleUrls: ['ng2flower.component.css']
+  styleUrls: ['ng2flower.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS]
 })
 export class Ng2flowerAppComponent {
   title = 'ng2flower works!';
